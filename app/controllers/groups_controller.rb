e21086f001e5040560d537ccb78bf04a25255ced
An Li <anli@Ans-Mac-mini.local>
2016-12-23 06:38:41 +0800

    fix some errors in find_groups_and_check_permission & add permission check on show page

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,6 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
-  before_action :find_group_and_check_permission, only[:edit, :update, :destroy]
+  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
   def index
     @groups = Group.all
   end
@@ -48,6 +48,7 @@ class GroupsController < ApplicationController
     params.require(:group).permit(:title, :description)
   end
   def find_group_and_check_permission
+    @group = Group.find(params[:id])
     if current_user != @group.user
       redirect_to root_path, alert: "You have no permission."
     end
